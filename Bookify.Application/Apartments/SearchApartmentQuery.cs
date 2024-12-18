3ea2f59ddc3fb00255eca6303dce47e74053baa9
Saman <samandaryaei@yahoo.com>
2024-12-18 22:15:39 +0330

    Created all application layer stuff which we needed.

---
+++ Bookify.Application/Apartments/SearchApartmentQuery.cs
@@ -0,0 +1,7 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Apartments.SearchApartments;
+
+namespace Bookify.Application.Apartments;
+
+public record SearchApartmentQuery(DateOnly StartDate, DateOnly EndDate)
+    :IQuery<IReadOnlyList<ApartmentResponse>>;
---
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -0,0 +1,7 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Apartments;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+public record ReserveBookingCommand(Guid apartmentId,Guid UserId,DateOnly StartDate,DateOnly EndDate) 
+    : ICommand<Guid>;
